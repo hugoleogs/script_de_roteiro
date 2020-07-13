c8215198fdf200e228df12346188be6d86df4a80
Hugo <hugoleonardo.gs2@gmail.com>
2020-07-13 22:51:39 +0000

    Editor de roteiro

---
+++ removToFloripa.py
@@ -0,0 +1,23 @@
+""" Esse script remove os 00 na frente do PExxxx para colocar em seguida do codigo do arquivo"""
+#script Broca_geral
+
+import os
+
+while(True):
+    try:
+        arqEntrada = ''.join(os.listdir('roteiro_afina/'))
+        with open(f'roteiro_afina/{arqEntrada}', encoding="utf8", errors='ignore') as f:
+            read_data = f.readlines()
+            print("Arquivo sendo Executado!")
+            saida = []
+            for i in read_data:
+                if i[2:4] == 'PE':
+                    saida.append(i[2:68]+'00'+i[68:])
+                else:
+                    saida.append(i)
+            with open(f"tofloripa/{arqEntrada}", "w") as arqSaida:
+                arqSaida.write(''.join(saida))
+            os.remove(f'roteiro_afina/{arqEntrada}')
+            print("Arquivo Finalizado!")
+    except:
+        print("Arquivo não Brocado!")
